24717f66b2805b45274afe78026bb2b95a876216
黄乐骏 <huanglejun@cangoonline.com>
2023-03-17 10:11:12 +0800

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,7 +144,7 @@ export default function Home() {
         <div className="flex items-center justify-center">
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
-            href="https://github.com/whoiskatrin/sql-translator"
+            href="https://github.com/hljpeter/sql-translator"
             target="_blank"
             rel="noopener noreferrer"
           >
